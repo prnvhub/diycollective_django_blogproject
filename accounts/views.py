c145917ead0d405e0530a4cd3081d6d3853e1894
prnvhub <74250536+prnvhub@users.noreply.github.com>
2021-11-22 15:22:13 +0530

    registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
 # Create your views here.
+
+def register(request):
+    if request.method=='POST':
+        first_name=request.POST['firstname']
+        last_name=request.POST['lastname']
+        username=request.POST['username']
+        email=request.POST['email']
+        password1=request.POST['password']
+        password2=request.POST['passwordconf']
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,"Username Taken")
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,"Email Already Exists")
+                return redirect('register')
+            else:   
+                user=User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password1)
+                user.save()
+                messages.info(request,"Success")
+                auth.login(request,user)
+        else:
+            messages.info(request,"Passwords Doesn't Match")
+        return redirect('/')
+    else:
+        return render(request,'register.html')
