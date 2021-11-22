6c7d58b6849f04312f89fe19eb645e0932f56575
prnvhub <74250536+prnvhub@users.noreply.github.com>
2021-11-22 16:05:34 +0530

    login&logout

--- accounts/views.py
+++ accounts/views.py
@@ -29,3 +29,21 @@ def register(request):
         return redirect('/')
     else:
         return render(request,'register.html')
+
+def login(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,"Invalid Details")
+            return redirect('login')
+    else:
+        return render(request,'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
